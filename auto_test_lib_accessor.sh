d0aad8e0387fad306039fd3f6ae57ae6c8513443
nakayama <nakayama@p.centsys.jp>
2023-10-06 11:23:27 +0000

    autoTestHasKeyValueの復活

--- auto_test_lib_accessor.sh
+++ auto_test_lib_accessor.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-function autoTestGetFirst() {
-    echo $(echo $1 | sed -n "s/.*\(\"${2}\":\) \"\([^\"]*\)\".*/\2/p")
-}
-
-function autoTestGetLast() {
-    echo $(echo $1 | sed -nE "s/.*\"${2}\":\s*\"([^\"]*)\".*/\1/p" | tail -n 1)
+function autoTestGet() {
+    echo $(echo $1 | jq -r $2)
 }
 
 function autoTestHasKeyValue() {
     echo $(echo $1 | sed -nE "s/.*\"${2}\":\s*\"(${3})\".*/\1/p" | tail -n 1)
 }
+
