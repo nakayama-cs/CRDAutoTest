7403949b8ce29c24d0e681202c925c59e3fa1ecd
nakayama <nakayama@p.centsys.jp>
2023-10-06 11:04:55 +0000

    grpcurlの返却値からjsonを抽出して返却する

--- auto_test_lib_exec.sh
+++ auto_test_lib_exec.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+function extractJson() {
+    echo $1 | sed -z "s/\n//g" | sed -n "s/.*Response contents:\s*\({.*}\).*/\1/p"
+}
+
 function autoTestExecGrpcurlList() {
     service=$1
     endpoint=$2
     json_file=$3
     host_info=$4
-    echo $(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/List${endpoint}s @${json_file} -u ${host_info} -s)
+    result=$(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/List${endpoint}s @${json_file} -u ${host_info} -s)
+    echo $(extractJson "$result")
 }
 
 function autoTestExecGrpcurlGet() {
@@ -13,7 +18,8 @@ function autoTestExecGrpcurlGet() {
     endpoint=$2
     json_file=$3
     host_info=$4
-    echo $(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/Get${endpoint} @${json_file} -u ${host_info} -s)
+    result=$(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/Get${endpoint} @${json_file} -u ${host_info} -s)
+    echo $(extractJson "$result")
 }
 
 function autoTestExecGrpcurlDelete() {
@@ -29,7 +35,8 @@ function autoTestExecGrpcurlUpdate() {
     endpoint=$2
     json_file=$3
     host_info=$4
-    echo $(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/Update${endpoint} @${json_file} -u ${host_info} -s)
+    result=$(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/Update${endpoint} @${json_file} -u ${host_info} -s)
+    echo $(extractJson "$result")
 }
 
 function autoTestExecGrpcurlCreate() {
@@ -37,5 +44,6 @@ function autoTestExecGrpcurlCreate() {
     endpoint=$2
     json_file=$3
     host_info=$4
-    echo $(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/Create${endpoint} @${json_file} -u ${host_info} -s)
+    result=$(go run ./cmd/mtechnavi-cli/ grpcurl ${service}/Create${endpoint} @${json_file} -u ${host_info} -s)
+    echo $(extractJson "$result")
 }
