bc1b4e98beb982b52e192e1ac894c1875cf59c86
nakayama <nakayama@p.centsys.jp>
2023-10-08 05:38:14 +0000

    配列内から特定キーの特定の値の存在の有無を検査するAPIを追加

--- auto_test_lib_accessor.sh
+++ auto_test_lib_accessor.sh
@@ -8,3 +8,33 @@ function autoTestHasKeyValue() {
     echo $(echo $1 | sed -nE "s/.*\"${2}\":\s*\"(${3})\".*/\1/p" | tail -n 1)
 }
 
+#
+# jsonを指定filterで抽出したデータに検査文字列が1つ以上含まれているかを検査します
+# 
+# 引数:
+#   $1 json文字列
+#   $2 jqのfilter (例: .items[].tokyoShokoResearchId)
+#   $3 検査文字列
+# 返却値:
+#   jsonを指定filterで抽出したデータに検査文字列が1つ以上含まれている場合はOK、それ以外は空文字を返却します。
+#
+function autoTestExistsValue() {
+    echo $(echo $1 | jq -r "${2}" | sed -n "s/^\(${3}\)$/OK/p")
+}
+
+#
+# jsonを指定filterで抽出したデータに検査文字列以外が含まれているかを検査します
+# 
+# 引数:
+#   $1 json文字列
+#   $2 jqのfilter (例: .items[].tokyoShokoResearchId)
+#   $3 検査文字列 (例1: "1234", 例2: "foo\|hoge")
+# 返却値:
+#   jsonの指定filterで抽出したデータに検査文字列が含まれていない場合はOK、それ以外は空文字を返却します。
+#
+function autoTestContainAnother() {
+    result=$(echo $1 | jq -r "${2}" | sed -z 's/\n/,/g' | sed "s/${3}//g" | sed "s/,//g")
+    if [ ! -z $result ]; then
+        echo "OK"
+    fi
+}
